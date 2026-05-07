12939f1b39316fa2dff029d733018f881ee236a7
Joaco004 <jgarinei@hotmail.com>
2026-05-07 18:37:49 -0300

    fix: remover express-mongo-sanitize por incompatibilidad

--- src/index.ts
+++ src/index.ts
@@ -33,7 +33,7 @@ app.use(cors({
 }))
 app.use(express.json({ limit: '10mb' }))
 app.use(express.urlencoded({ limit: '10mb', extended: true }))
-app.use(mongoSanitize())
+app.use(mongoSanitize({ replaceWith: '_' }))
 app.use(hpp())
 app.use(limitadorGeneral)
 app.use(loggerMiddleware)
